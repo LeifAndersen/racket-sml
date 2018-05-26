146a28689a6480183ee8ed4c4da55a77d1f98d06
Leif Andersen <leif@leifandersen.net>
2018-05-26 10:03:28 -0400

    Add at-exp-lib dep.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,8 @@
 #lang info
 
 (define collection "sml")
-(define deps '("base"))
+(define deps '("base"
+               "at-exp-lib"))
 (define pkg-desk "S-Markup Language")
 (define version "0.0")
 (define pkg-authors '(leif))
